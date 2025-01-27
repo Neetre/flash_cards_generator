8b6f1dc744b7dd7ed39ab7768ca3e82865fe03d7
Mattia Braga <mattiabraga2006@gmail.com>
2025-01-27 08:47:31 +0100

    update

--- bin/libs_py/api.py
+++ bin/libs_py/api.py
@@ -47,9 +47,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["http://fc.figliolo.it"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 class Config:
